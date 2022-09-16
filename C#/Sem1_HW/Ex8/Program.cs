b3501085382384164b10bd07d6e81684ddf2579d
MaratA <mister.asylbaeff@gmail.com>
2022-09-16 16:48:00 +0300

    HW_S1

---
+++ C#/Sem1_HW/Ex8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите положительное число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+num1 = Math.Abs(num1); //Защита от бесконечного цикла
+int i = 2;
+while (i <= num1)
+{
+    Console.Write($"{i}, ");
+    i = i + 2;
+}
