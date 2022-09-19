d9709baa789a19f6e08189620ab144976f8054e0
MaratA <mister.asylbaeff@gmail.com>
2022-09-19 22:33:51 +0300

    sem2

---
+++ C#/Sem2/Ex1/Program.cs
@@ -0,0 +1,40 @@
+﻿void zadacha2()
+{
+    //Ввод значений
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Сравнение и вывод
+if (num1 > num2)          
+{
+    Console.WriteLine($"max={num1}, min={num2}");
+}
+else 
+{
+    Console.WriteLine($"max={num2}, min={num1}");
+}
+
+}
+
+
+void zadacha6()
+{
+    //Ввод значения
+    Console.WriteLine("Введите положительное число");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    //Задание первого значения
+    int i = 2;
+
+    //Цикл проверки условия и вывода
+    while (i <= num1)
+    {
+        Console.Write($"{i}, ");
+        i = i + 2;
+    }
+}
+
+zadacha2();
+zadacha6();
---
+++ C#/Sem2/Ex9/Program.cs
@@ -0,0 +1,88 @@
+﻿void Zadacha9()
+{
+    Random random = new Random();
+    int number = random.Next(10,100);
+    Console.WriteLine(number);
+    int tens = number / 10;
+    int ones = number % 10;
+    Console.WriteLine(tens);
+    Console.WriteLine(ones);
+}
+
+void Zadacha11()
+{
+    Random random = new Random();
+    int number = random.Next(100,1000);
+    Console.WriteLine(number);
+    int hundreds = number / 100;
+    int ones = number % 10;
+    int result = hundreds * 10 + ones;
+    Console.WriteLine(result);
+    
+
+}
+
+
+void Zadacha13()
+{
+    Console.WriteLine("Введите первое число");     
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+    if (num2 % num1 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine($"Не кратно, остаток = {num2 % num1}");
+    }
+
+}
+
+ 
+ 
+ void Zadacha14()
+{
+    Console.WriteLine("Введите число");     
+    int num1 = Convert.ToInt32(Console.ReadLine());
+     
+
+    if (num1 % 7 == 0 && num1 % 23 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+
+}
+
+
+void Zadacha15()
+{   
+    int input()
+    {
+      Console.WriteLine("Введите первое число");     
+      return Convert.ToInt32(Console.ReadLine());  
+    }
+         
+    int num1 = input();
+    int num2 = input(); 
+
+    if (num1 == Math.Pow(num2, 2) || num2 == num1 * num1)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+
+}
+
+Zadacha15();
+
+
