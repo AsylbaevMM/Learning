89ba18721debcab4e53f4c5ee05532700ab374b6
MaratA <mister.asylbaeff@gmail.com>
2022-09-22 23:39:55 +0300

    Sem3_HW

---
+++ C#/Sem3/Program.cs
@@ -0,0 +1,147 @@
+﻿
+
+void zadacha17()
+{
+    Console.WriteLine("Задача 17");
+
+    Random random = new Random();
+
+    int x = random.Next(-10, 11);
+    int y = random.Next(-10, 11);
+    Console.WriteLine($"A{x}, {y}");
+    CoordCheck(x, y);
+}
+
+
+void CoordCheck(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine($"a{x}, {y} Лежит в первой");
+    }
+
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine($"a{x}, {y} Лежит во второй");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine($"a{x}, {y} Лежит в третьей");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine($"a{x}, {y} Лежит в четветрой");
+    }
+    else
+    {
+        Console.WriteLine($"a{x}, {y} Не относится ни к одной");
+    }
+}
+
+
+
+void zadacha18()
+
+{
+    Console.WriteLine("Задача 18");
+    Console.WriteLine("Введите четверть");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Check(num);
+}
+
+
+void Check(int num)
+{
+    if (num == 1)
+    {
+        Console.WriteLine($"В первой четверти х в диапазоне (0; +∞), у в диапазоне (0; +∞)");
+    }
+
+    else if (num == 2)
+    {
+        Console.WriteLine($"Во второй четверти х в диапазоне (-∞; 0), у в диапазоне (0; +∞)");
+    }
+    else if (num == 3)
+    {
+        Console.WriteLine($"В третьей четверти х в диапазоне (-∞; 0), у в диапазоне (-∞; 0)");
+    }
+    else if (num == 4)
+    {
+        Console.WriteLine($"В четвертой четверти х в диапазоне (0; +∞), у в диапазоне (-∞; 0)");
+    }
+    else
+    {
+        Console.WriteLine($"Неверный ввод");
+    }
+}
+
+
+
+
+
+void zadacha21()
+{
+    Console.WriteLine("Задача 21");
+
+    Random random = new Random();
+
+    int x1 = random.Next(0, 100);
+    int y1 = random.Next(0, 100);
+    int x2 = random.Next(0, 100);
+    int y2 = random.Next(0, 100);
+
+    Console.WriteLine($"A {x1}, {y1}");
+    Console.WriteLine($"B {x2}, {y2}");
+    Dist(x1, y1, x2, y2);
+}
+
+
+void Dist(int x1, int y1, int x2, int y2)
+{
+
+    Console.WriteLine($"Расстояние между точками = {Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2))}");
+
+
+}
+
+
+//zadacha21();
+//zadacha18();
+//zadacha17();
+
+
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+void zadacha22()
+
+{
+    Console.WriteLine("Задача 22");
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    result(num);
+}
+
+void result(int num)
+{
+    int i = 1;
+    if (num > 1)
+    {
+        while (i <= num)
+           {
+             Console.WriteLine($"Квадрат числа {i} = {Math.Pow((i), 2)}");
+           i++;
+           }
+    }
+    else
+    {
+         while (num <= 1)
+           {
+             Console.WriteLine($"Квадрат числа {num} = {Math.Pow((num), 2)}");
+           num++;
+           }
+    }
+
+}
+
+zadacha22();
---
+++ C#/Sem3_HW/Program.cs
@@ -0,0 +1,105 @@
+﻿
+void zadacha19()
+
+{
+    // Взаимодействие с пользователем и ввод
+    Console.WriteLine("Задача 19");
+    Console.WriteLine("Введите пятизначное число");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (Math.Abs(num) > 9999 && Math.Abs(num) < 100000)  // проверка на пятизначность
+    {
+        if (num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10)  //проверка на равенство противостоящих чисел
+        {
+            Console.WriteLine("Число является палиндромом");   // вывод
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Это не пятизначное число");
+    }
+}
+
+
+//____________________________________________________________________________________
+
+
+void zadacha21()
+{
+    Console.WriteLine("Задача 21");
+
+    //Создание случайных точек
+    Random random = new Random();   
+
+    int x1 = random.Next(0, 100);
+    int y1 = random.Next(0, 100);
+    int z1 = random.Next(0, 100);
+    int x2 = random.Next(0, 100);
+    int y2 = random.Next(0, 100);
+    int z2 = random.Next(0, 100);
+
+    //Вывод координат точек
+    Console.WriteLine($"A {x1}, {y1}, {z1}");
+    Console.WriteLine($"B {x2}, {y2}, {z2}");
+    
+    //Функция для нахождения квадрата разности координат
+    int sumsqr(int num1, int num2)
+    {
+
+    int result = Convert.ToInt32(Math.Pow((num1 - num2), 2));
+    return result;
+        
+    }
+
+    //Математика и вывод расстояния
+    Console.WriteLine($"Расстояние между точками = {Math.Sqrt(sumsqr(x1, x2) + sumsqr(y1, y2) + sumsqr(z1, z2))}");
+
+}
+
+
+//______________________________________________________________________________________________
+
+
+void zadacha23()
+
+{   
+    //Взаимодействие с пользователем и ввод
+    Console.WriteLine("Задача 23");
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Число\tКуб"); //вывод заголовка таблицы
+    result(num);
+}
+
+//проверка числа положительное/отрицательное, циклы и вывод
+void result(int num)
+{
+    int i = 1;
+    if (num > 1)
+    {
+            
+        while (i <= num)
+           {
+             Console.WriteLine("{0}\t{1}", i, Math.Pow(i, 3));
+           i++;
+           }
+    }
+    else
+    {
+           while (i >= num)
+           {
+             Console.WriteLine("{0}\t{1}", i, Math.Pow(i, 3));
+           i--;
+           }
+    }
+
+}
+
+
+zadacha19();
+zadacha21();
+zadacha23();
