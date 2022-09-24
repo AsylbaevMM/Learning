b3043caea611aedd7b9db5c7f018c24a75024b9f
MaratA <mister.asylbaeff@gmail.com>
2022-09-24 15:08:40 +0300

    lec3

---
+++ C#/Lec3/Ex1/Program.cs
@@ -0,0 +1,58 @@
+﻿//Не принимают, не возвращают
+void Method1() 
+{
+    Console.WriteLine("Автор ...");
+}
+//Method1();
+
+// Только принимают
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21("текст", 12);
+//Method21(msg: "текст", count: 12);
+
+
+//Ничего не принмают, что-то возвращают
+int Method3()
+{
+
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+//Console.WriteLine(year);
+
+//Принимают, возвращают
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; //Создаем пустую строку
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "пример");
+Console.WriteLine(res);
+
+
+
+
+
+
---
+++ C#/Lec3/Ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿string text = "олупрвмл толыв. - лоыуар олырп, ыфклпо - "
+            + "цау ца.ац кп цуас, ыралв- мцкопыват"
+            + "укып уумыксро, влаоыс. каыо-цаьшщр";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '_');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
