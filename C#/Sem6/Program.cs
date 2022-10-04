d49ef544f5217ce6bf2dcb34295146da7399a2df
MaratA <mister.asylbaeff@gmail.com>
2022-10-04 21:55:38 +0600

    Sem6_HW

---
+++ C#/Sem6/Program.cs
@@ -0,0 +1,129 @@
+﻿void  FillArray(int[] numbers,
+                int minValue = 0,
+                int maxValue = 9)
+{
+    int size = numbers.Length;
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = random.Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.WriteLine("Вывод массива:");
+    int size = numbers.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int[] numbers)
+{
+    int size = numbers.Length;
+    int maxIndex = size - 1;
+    for (int i = 0; i < size/2; i++)
+    {
+        // int temp = numbers[i];
+        // numbers[i] = numbers[maxIndex - i];
+        // numbers[maxIndex - i] = temp;
+        (numbers[i], numbers[maxIndex - i]) = (numbers[maxIndex - i], numbers[i]);
+    }
+}
+
+void zadacha39()
+{
+    int size = 10;
+    int[] numbers = new int[size];
+    FillArray(numbers, -10, 10);
+    PrintArray(numbers);
+    ReverseArray(numbers);
+    PrintArray(numbers);
+}
+
+//zadacha39();
+
+void zadacha40()
+{
+    Console.WriteLine("Введите длину стороны А");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите длину стороны B");
+    int B = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите длину стороны C");
+    int C = Convert.ToInt32(Console.ReadLine());
+    if (A < (B + C) && B < (A + C) && C < (A + B))
+    {
+        Console.WriteLine("Треугольник существует");
+    }
+    else
+    {
+        Console.WriteLine("Треугольник не существует");
+    }
+}
+
+//zadacha40();
+
+void zadacha42()
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string result = "";
+    int temp;
+    //int num = 
+    while (number > 0)
+    {
+        result =  number%2 + result;
+        number /= 2;                                                                                   
+    }
+    
+    Console.WriteLine(result);
+}
+//zadacha42();
+
+void zadacha42a()
+{
+    int number = 43;
+    string result = Convert.ToString(number, 2);
+    Console.WriteLine(result);
+}
+//zadacha42a();
+
+
+void zadacha44()
+{
+    Console.WriteLine("Введите число");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int a = 0;
+    int b = 1;
+    int[] numbers = new int[size];
+    numbers[0] = a;
+    numbers[1] = b;
+    for (int i = 2;  i < numbers.Length; i++)
+    {
+        numbers[i] = numbers[i-2] + numbers[i -1];
+    }
+    PrintArray(numbers);
+}
+//zadacha44();
+
+
+void zadacha45()
+{
+    int size = 10;
+    int[] numbers1 = new int[size];
+    int[] numbers2 = new int[size];
+
+    FillArray(numbers1);
+    for (int i = 0; i < numbers2.Length; i++)
+    {
+        numbers2[i] = numbers1[i];
+    }
+    PrintArray(numbers1);
+    PrintArray(numbers2);
+}
+zadacha45();
+
+
---
+++ C#/Sem6_HW/Program.cs
@@ -0,0 +1,76 @@
+﻿void PrintArray(int[] numbers)
+{
+    Console.WriteLine("Вывод массива:");
+    int size = numbers.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+void zadacha41()
+{
+    //Взаимодействие с пользователем
+    Console.WriteLine("Введите любое количество чисел");
+    Console.WriteLine("После каждого числа нажимайте Enter");
+    Console.WriteLine("После ввода всех чисел нажмите Enter два раза");
+
+    // Объявление переменных
+    int[] numbers = new int[0];
+    int i = 0;
+    int count = 0;
+    int countless0 = 0;
+
+    //Тут создаю рекурсию, выход из которой происходит при получении пустой строки(нажатии Enter без ввода)
+    void input()
+    {
+        string temp = Console.ReadLine();
+        if (temp == "") return;
+        Array.Resize(ref numbers, numbers.Length + 1);
+        numbers[i] = Int32.Parse(temp);
+        //Array.Resize(ref numbers, numbers.Length + 1);
+        i++;
+        count++;
+        if (Int32.Parse(temp) > 0) countless0++;
+        input();
+
+    }
+    input();
+    PrintArray(numbers);
+    Console.WriteLine();
+    Console.WriteLine($"Вы ввели {count} чисел, {countless0} из них больше нуля");
+    Console.WriteLine();
+}
+
+
+
+
+void zadacha43()
+{
+    Console.WriteLine("Введите параметр К1:");
+    double K1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите параметр B1:");
+    double B1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите параметр К2:");
+    double K2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите параметр B2:");
+    double B2 = Convert.ToInt32(Console.ReadLine());
+
+    if (K1 - K2 != 0)
+    {
+    double X = Math.Round((B2 - B1) / (K1 - K2), 2);
+    double Y = Math.Round(K1*X + B1, 2);
+
+    Console.WriteLine($"Координаты точки пересечения прямых: {X} , {Y}");
+    }
+    else
+    {
+     Console.WriteLine("Прямые параллельны");   
+    }
+}
+
+//zadacha41();
+zadacha43();
