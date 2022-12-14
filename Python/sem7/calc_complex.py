c19847e2089c5ef46aea0c7662c00e71f424dc4a
Andrew <it.andr@outlook.com>
2022-12-14 17:00:58 +0300

    add complex

--- Python/sem7/calc_complex.py
+++ Python/sem7/calc_complex.py
@@ -1,2 +1,15 @@
 def calc_complex_main(a, b, move):
-    pass
+    if move == '*':
+        return a * b
+    if move == '/':
+        return a / b
+    if move == '+':
+        return a + b
+    if move == '-':
+        return a - b
+
+num1 = input('Введите первое число: ')
+num2 = input('Введите комплексное число (Пример 4j): ')
+oper = input('Введите операцию над комплексными числами: ')
+
+calc_complex_main(num1, num2, oper)
