fc94b27de009199c9defa288c78f0af678cd1dcc
Alexey <Alexey.gt@bk.ru>
2022-12-14 14:25:06 +0300

    Исправление log

--- Python/sem7/log.py
+++ Python/sem7/log.py
@@ -1,2 +1,4 @@
-def log_main(info):
-    pass
+from datetime import datetime 
+def log_main(string):
+    with open ('output.txt','a') as output_file:
+        output_file.write(datetime.now().strftime('Year:%Y Month:%m Day:%d Hour:%H Minute:%M Second:%S')+string)
