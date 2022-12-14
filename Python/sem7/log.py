b8870d3c95c6099281d4e26aba4024ba4f2c2b73
MaratA <mister.asylbaeff@gmail.com>
2022-12-14 15:47:43 +0300

    updated main

--- Python/sem7/log.py
+++ Python/sem7/log.py
@@ -2,3 +2,4 @@ from datetime import datetime
 def log_main(string):
     with open ('output.txt','a') as output_file:
         output_file.write(datetime.now().strftime('Year:%Y Month:%m Day:%d Hour:%H Minute:%M Second:%S')+string)
+
--- Python/sem7/main.py
+++ Python/sem7/main.py
@@ -2,12 +2,15 @@
 from calc_fraction import calc_fraction_main
 from calc_complex import calc_complex_main
 from log import log_main
+import os
+
+
 
 def variable_input():
     a = input("Введите первое число >>> ")
     b = input("Введите второе число >>> ")
     move = input("Введите действие >>> ")
-    while move not in '+-*/':
+    while move not in ['+', '-', '*', '/']:
         print('Вы ввели некорректное действие')
         move = input("Введите корректное действие >>> ")
     return a, b, move
@@ -24,18 +27,21 @@ def result_string(func, a, b, move):
 def main():
     choice = input("Выберите тип чисел: 1.Комплексные, 2.Рациональные >>> ")
 
+    while choice not in '12':
+        print('Вы ввели некорректное значение')
+        choice = input("Выберите тип чисел: 1.Комплексные, 2.Рациональные >>> ")
+
     if choice == '1':
         print("Вы выбрали комплексные числа. Введите числа в формате 'a + bj' >>> ")
         result = result_string(calc_complex_main, *variable_input())
         final(result)
 
-    elif choice == '2':
-        print("Вы выбрали рациональные числаю Введите числа в формате 'a/b' >>> ")
+    if choice == '2':
+        print("Вы выбрали рациональные числа. Введите числа в формате 'a/b' >>> ")
         result = result_string(calc_fraction_main, *variable_input())
         final(result)
 
-    else:
-        print('Вы ввели неверное значение')    
+   
 
     again = input('Для нового подсчета введите 1, для выхода нажмите Enter >>> ')
     
