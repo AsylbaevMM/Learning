d7ad3c270eb92508cfb791f91e9c4f99322939e3
Alexey <Alexey.gt@bk.ru>
2022-12-17 17:00:32 +0300

    Reader

--- Python/sem8/reader.py
+++ Python/sem8/reader.py
@@ -1,2 +1,6 @@
 def reader_main(family):
-    pass
+   with open ('data.txt','r') as data:
+        list = data.read().split(' 1')
+        for i in range(len(list)):
+            if family not in list:
+                return list[i] 
