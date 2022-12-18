23ca254084b6a13b61ced343cdc805f7bd0197d7
Alexey <Alexey.gt@bk.ru>
2022-12-18 15:35:22 +0300

    reader-fix

--- Python/sem8/reader.py
+++ Python/sem8/reader.py
@@ -1,16 +1,13 @@
 def reader_main(family):
     
     with open ('data.txt','r') as data:
+        result = []
         spis = []
         list = data.read().split('\n')
-        flag = 0
         for i in range(len(list)):
             if family in list[i]:
                 spis = list[i].split('<word>')
-                #return spis #вывод 1 раз
-                flag = 1
-
-            if flag == 0:
-                print('Введены не корректные данные в поиске!!!')
-        return spis   
+                result.append(spis)
+               
+        return result   
 print(reader_main('Иванов'))
