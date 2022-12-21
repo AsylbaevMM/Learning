8ead0a12ea64d8605c72ec7afc5cb6c6e64a014d
MaratA <mister.asylbaeff@gmail.com>
2022-12-21 08:52:07 +0300

    sem9

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,11 +1,5 @@
-def writer_main(args):
-    with open ('data.txt','a') as data:
-        print(f'{"<word>".join(args)}', file = data)
+from isOdd import isOdd
 
 
-def reader_main(word):
-    with open ('data.txt','r') as data:
-        list = data.read().split('\n')
-        for i in list:
-            if word in i:
-                return i.split('<word>')
+print(isOdd(1)) #=> false
+print(isOdd(4)) #=> false
