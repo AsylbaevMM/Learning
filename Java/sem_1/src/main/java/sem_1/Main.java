50e9f7400a684d55c8e5d5a413c6282ed506087b
AsylbaevMM <110394648+AsylbaevMM@users.noreply.github.com>
2023-01-25 00:27:22 +0300

    Update Main.java

--- Java/sem_1/src/main/java/sem_1/Main.java
+++ Java/sem_1/src/main/java/sem_1/Main.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //ex1();
-        //ex2();
+        ex1();
+        ex2();
         ex3();
     }
 
