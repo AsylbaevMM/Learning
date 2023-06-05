17beeb1f2b5d353edaf603c25e3f1decc8c28232
MaratA <mister.asylbaeff@gmail.com>
2023-06-05 18:18:06 +0300

    algs_sem4_HW

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,9 +1,17 @@
 
 
-class Ex:
-    def __init__(self, b):
-        self.b = b
+stroka1='пара-ра-рам рам-пам-папам па-ра-па-дам'
+vowels=['a','e','ё','и','й','o','у','ы','э','ю','я']
+phrases=stroka1.split()
+if len(phrases)<2:
+   print('Количество фраз должно быть больше одной!')
+else:
+   countVowels=[]
 
-obj = Ex(3)
+   for i in phrases:
+       countVowels.append(len([x for x in i if x.lower() in vowels]))
 
-print(obj.__dict__)
+if len (set(countVowels))==1:
+   print('Парам пам-пам')
+else:
+   print('Пам парам')
