1c861c14eff807037cf8c7e0de7c4956b365b64e
MaratA <mister.asylbaeff@gmail.com>
2023-06-22 11:55:32 +0300

    network sem6 HW

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,9 +1,45 @@
-import random
-import pandas as pd
+def spiral_transposition(matrix):
+    n = len(matrix)
+    m = len(matrix[0])
+    i = 0
+    j = 0
 
-lst = ['robot'] * 10
-lst += ['human'] * 10
-random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
-data.head()
-    
+    cnt = 1
+    result = []
+    if not n*m:
+        return result
+
+    while cnt < n*m:
+        while j < m - 1 and matrix[i][j + 1] != None:
+            result.append(matrix[i][j])
+            matrix[i][j] = None
+            j += 1
+            cnt += 1
+
+        while i < n - 1 and matrix[i + 1][j] != None:
+            result.append(matrix[i][j])
+            matrix[i][j] = None
+            i += 1
+            cnt += 1
+
+        while j > 0 and matrix[i][j - 1] != None:
+            result.append(matrix[i][j])
+            matrix[i][j] = None
+            j -= 1
+            cnt += 1
+
+        while i > 0 and matrix[i - 1][j] != None:
+            result.append(matrix[i][j])
+            matrix[i][j] = None
+            i -= 1
+            cnt += 1
+
+    result.append(matrix[i][j])
+    return result
+
+
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+          
+print(spiral_transposition(matrix))
--- sandbox/sandbox2.py
+++ sandbox/sandbox2.py
@@ -1,13 +1,3 @@
 
 
-
-
-def squareList(num):
-    while num > 0:
-        yield num**2
-        num -= 1
-
-square = squareList(10)
-
-for i in range(11):
-    print(next(square))
+print(dir(tuple))
