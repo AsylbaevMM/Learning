03bb54654c062cbd3a74dd83163e00a3e7fc1733
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-08-04 21:22:35 +0300

    sem10

---
+++ Python/vol2_sem10/main.py
@@ -0,0 +1,58 @@
+# Создайте три (или более) отдельных классов животных.
+# Например рыбы, птицы и т.п.
+# У каждого класса должны быть как общие свойства,
+# например имя, так и специфичные для класса.
+# Для каждого класса создайте метод, выводящий
+# информацию специфичную для данного класса.
+
+class Animal:
+    def __init__(self, name, age, voice = 'groal'):
+        self.name = name
+        self.age = age
+        self.voice = voice
+    
+    def make_voice(self):
+        print(self.voice)
+
+
+
+class Fish(Animal):
+    def __init__(self, name, age, scales, voice):
+        super().__init__(name, age, voice)      
+        self.scales = scales
+
+
+    def swim(self):
+        print("i'm swimming, oh, it's titan!")
+
+
+class Dog(Animal):
+    def __init__(self, name, age, breed, voice):
+        super().__init__(name, age, voice)  
+        self.breed = breed
+
+    def bark(self):
+        print('Bark!')
+
+
+class Raven(Animal):
+    def __init__(self, name, age, color, voice):
+        super().__init__(name, age)
+        self.voice = voice
+        self.color = name
+    
+    def fly_around_corpse(self):
+        print('oooh, meat....')
+
+fish = Fish('Nemo', 2, 'silver', 'bul-bul')
+dog = Dog('Spark', 5, 'pitbull', 'bark!')
+bird = Raven('Qarasique', 6, 'white', 'bravo!')
+
+animals = [fish, dog, bird]
+
+for i in animals:
+    i.make_voice()
+
+fish.swim()
+dog.bark()
+bird.fly_around_corpse()
