ccff3d822d2b59dab2bf191f17e04bb4e4e33530
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-09-04 16:15:27 +0300

    sem2_HW_update2

--- flask/sem2/app.py
+++ flask/sem2/app.py
@@ -17,18 +17,23 @@ def index():
 def login():
     context = {'title': 'Авторизация' }
     if request.method == 'POST':
-        session['username'] = request.form.get('username') or 'NoName'
-        session['email'] = request.form.get('email') or 'NoEmail'
-        return redirect(url_for('greating'))
+        response = make_response(redirect(url_for('greating')))
+        name = request.form.get('username') or 'NoName'
+        response.set_cookie('username', str(name))
+        email = request.form.get('email') or 'NoEmail'
+        response.set_cookie('email', str(email))
+        return response
     return render_template('login_form.html')
 
 @app.route('/greating/')
 def greating():
-    context = {'title': 'Приветсвие', 'username': session['username'], 'email': session['email']}
+    context = {'title': 'Приветсвие', 'username': request.cookies.get('username'), 'email': request.cookies.get('email')}
     return render_template('greating.html', **context)
 
 
 @app.route('/logout/')
 def logout():
-    session.clear()
-    return redirect(url_for('index'))
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie('email', 'None', max_age = 0)
+    response.set_cookie('username', 'None', max_age=0)
+    return response
