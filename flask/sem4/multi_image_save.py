11ee35978103d1fb21832776110dbb981272217a
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-09-08 15:10:00 +0300

    sem4_ HW

---
+++ flask/sem4/multi_image_save.py
@@ -0,0 +1,36 @@
+import requests
+from multiprocessing import Process, Pool
+import time
+import sys
+from random import sample
+
+LINK = sys.argv
+if len(LINK) > 1:
+    LINK = LINK[1]
+else:
+    LINK = 'links.txt'
+
+
+def download(url):
+    start_time = time.time()
+    response = requests.get(url)
+    image_name = url.split('/')[-1]
+    filename = f'multi_images/{image_name}'
+    with open(filename, "wb") as f:
+        f.write(response.content)
+    print(f"Downloaded {url} in {time.time() - start_time:.2f} seconds")
+processes = []
+
+with open(LINK, 'r', encoding='utf-8') as file:
+    links = [i.rstrip() for i in file.readlines() if i.lower().endswith('.jpg\n')]
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    for url in sample(links, min(10, len(links))):
+        process = Process(target=download, args=(url,))
+        processes.append(process)
+        process.start()
+    for process in processes:
+        process.join()
+    print(f"Total {time.time() - start_time}")
