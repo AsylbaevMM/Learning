1ed9502c8973fe999b9366478add951812a2bdea
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-12-10 16:16:04 +0300

    UT sem 5,6 HW

--- sandbox/sandbox2.py
+++ sandbox/sandbox2.py
@@ -1,30 +1,15 @@
+from pprint import pprint
 
+import openpyxl
 
+wb = openpyxl.load_workbook('roles.xlsx')
 
-class Repo():
-    def __init__(self,number, parent = None):
-        self.number = number
-        self.parent = parent
-        self.childs = []
-        self.childs_count = 0
-        if parent is not None:
-            parent.childs.append(self)
-            self.parent.add_child()
+sheet = wb.active
 
-    def find_longer(self):
-        if self.childs:
-            max(self.childs, key = lambda x: x.childs_count).find_longer()
-        else:
-            print(self.number)
+result = {}
 
-    def add_child(self):
-        self.childs_count += 1
-        if self.parent is not None:
-            self.parent.add_child()
-    
-repos = [Repo(0)]
+for row in sheet:
+    if row[4].value != 'no':
+        result[row[2].value] = row[5].value
 
-for i in range(int(input())):
-    repos.append(Repo(i+1, repos[int(input())]))
-
-repos[0].find_longer()
+pprint(result)
--- sandbox/sandbox3.py
+++ sandbox/sandbox3.py
@@ -1,16 +1,15 @@
 
 
 
-n, c, d = [int(i) for i in input().split()]
-nums = [int(i) for i in input().split()]
+PERMISSIONS_WITH_DESCRIPTIONS = {
+    'PERM_1_CODENAME': ['perm 1 name', 'Description for permission 1'],
+    'PERM_2_CODENAME': ['perm 2 name', 'Description for permission 2'],
+    'PERM_3_CODENAME': ['perm 3 name', 'Description for permission 3'],
+    'PERM_4_CODENAME': ['perm 4 name', 'Description for permission 4'],
+    'PERM_5_CODENAME': ['perm 5 name', 'Description for permission 5'],
+    }
 
-while c:
-    # уменьшаем минимальное число, пока не станет меньше нуля
-    while c and not [i for i in nums if i < 0]: 
-        nums[nums.index(min(nums))] -= d
-        c -= 1
-    # увеличиваем минимальное положительное число, это в произведении с отрицательным даст наименшее возможное число
-    if c:
-        nums[nums.index(min([i for i in nums if i > 0]))] += d
-        c -= 1
-print(*nums)
+# это можно передать в метакласс
+PERMISSIONS = [(codename, info[0]) for codename, info in PERMISSIONS_WITH_DESCRIPTIONS.items()]
+
+print(*PERMISSIONS, sep='\n')
