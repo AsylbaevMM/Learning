4108aa1318fabbb7f5527b1f59d498ae53e78362
AsylbaevMM <mister.asylbaeff@gmail.com>
2024-01-28 19:25:14 +0300

    paradigms sem4 HW

--- sandbox/sandbox3.py
+++ sandbox/sandbox3.py
@@ -1,11 +1,3 @@
 
 
-from datetime import date
 
-iters = int(input())
-for i in range(iters):  
-    try:
-        obj = date(*[int(i) for i in input().split()[::-1]])
-        print('YES')
-    except:
-        print('NO')
